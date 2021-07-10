1a219676a7de2d26acc80503d799c4521b72beee
WilliamYWang <williamwang0223@gmail.com>
2021-07-09 20:10:20 -0400

    Completed event creation endpoint and added react app

--- backend/controllers/handlers.go
+++ backend/controllers/handlers.go
@@ -1,36 +1,40 @@
 package controllers
 
 import (
-	// "github.com/codeday-labs/2021_event_lottery/models"
+	"github.com/codeday-labs/2021_event_lottery/models"
+	"github.com/codeday-labs/2021_event_lottery/database"
 	"github.com/gofiber/fiber/v2"
-	// "fmt"
-	// "strconv"
-	// "time"
+	"fmt"
+	"strconv"
+	"time"
 )
 
 func Create(c *fiber.Ctx) error {
-	var data map[string]string
+	var data map[string] string
 
 	if err := c.BodyParser(&data); err != nil {
 		return err
 	}
 
-	// // Parsing Body
-	// maxAttendees, err := strconv.Atoi(data["maxAttendees"])
-	// if err != nil {
-    //     fmt.Println(err)
-    // }
+	// Parsing Body
+	maxAttendees, err := strconv.Atoi(data["maxAttendees"])
+	if err != nil {
+        fmt.Println(err)
+    }
+	// FIX TIME
 	// layout := "2021-07-14 19:58:00"
 	// startTime, _ := time.Parse(layout, data["startTime"])
 	// endTime, _ := time.Parse(layout, data["endTime"])
 
-	// event := models.Event {
-	// 	EventName: data["eventName"],
-	// 	LotteryTime: time.Now(),
-	// 	MaxAttendees: maxAttendees,
-	// 	StartTime: startTime,
-	// 	EndTime: endTime,
-	// }
+	event := models.Event {
+		EventName: data["eventName"],
+		LotteryTime: time.Now(),
+		MaxAttendees: maxAttendees,
+		StartTime: time.Now(),
+		EndTime: time.Now(),
+	}
 
-	return c.JSON(data)
+	database.Connection.Create(&event)
+
+	return c.JSON(event)
 }
---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+function App() {
+  return (
+    <div className="App">
+      <ul>
+        <li>
+          <a href="eventcreate.html">Event Creation</a>
+        </li>
+        <li>
+          <a href="eventregister.html">Event Registration</a>
+        </li>
+        <li>
+          <a href="events.html">Current Events</a>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
