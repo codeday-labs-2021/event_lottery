17036ffa8b565c4907b7858f18bcf12bb47916f6
WilliamYWang <williamwang0223@gmail.com>
2021-07-11 18:47:11 -0400

    Set up redirection and react frontend pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,32 @@
-function App() {
-  return (
-    <div className="App">
-      <ul>
-        <li>
-          <a href="eventcreate.html">Event Creation</a>
-        </li>
-        <li>
-          <a href="eventregister.html">Event Registration</a>
-        </li>
-        <li>
-          <a href="events.html">Current Events</a>
-        </li>
-      </ul>
-    </div>
-  );
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Home } from './pages/Home';
+import { Create } from './pages/Create';
+import { Register } from './pages/Register';
+import { Events } from './pages/Events';
+import { NoMatch } from './pages/NoMatch';
+import { Layout } from './components/Layout';
+import { NavBar } from './components/Nav';
+
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <NavBar/>
+        <Layout>
+          <Router>
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/create" component={Create} />
+              <Route exact path="/register" component={Register} />
+              <Route exact path="/events" component={Events} />
+              <Route component={NoMatch} />
+            </Switch>
+          </Router>
+        </Layout>
+      </React.Fragment>
+    )
+  }
 }
 
 export default App;
---
+++ frontend/src/pages/Register.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Register = () => {
+  return (
+    <div>
+      <h1>Register Page</h1>
+    </div>
+  );
+};
