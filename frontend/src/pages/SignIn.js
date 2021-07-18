0856bbe7a82e0151d5ac933b9553e4d76c0a241c
Abenezer T <abety8@outlook.com>
2021-07-18 02:18:09 -0700

    usercreation/login ui

---
+++ frontend/src/pages/SignIn.js
@@ -0,0 +1,75 @@
+import React, { useState } from "react";
+import { Form, Col, Button,FormGroup } from "react-bootstrap";
+import { useHistory, Redirect } from "react-router-dom";
+import axios from "axios";
+
+export const SignIn = () => {
+
+  const[input, setInput]=useState({
+    email:"",
+    password:""
+  })
+  const handleChanges=(event)=>{
+    const {name,value}=event.target;
+    setInput({
+      ...input,
+      [name]:value,
+      
+    });
+  }
+    const handleSubmit = e => {
+      e.preventDefault();
+     
+    const requestConfig={
+      url:'http://127.0.0.1:4001/api/v1/singnin',
+      method:'post',
+      header:{'Content-Type': 'application/jason'},
+      data:{
+        email:input.email,
+        password:input.password,
+      },
+    };
+    axios(requestConfig)
+    .then((response)=>{
+      console.log(response);
+    })
+     .catch((err)=>{
+       console.log(`${err}`);
+     });
+
+    }
+
+     
+
+    return (  
+    <div className="App"  as={Col} xs="2">
+         
+        <Form className="form" onSubmit={handleSubmit}>
+        <h2 className="title">SignIn</h2>
+          <FormGroup>
+          <Form.Label>Enter Email</Form.Label>
+            <Form.Control
+              required
+              type="email"
+              name="email"
+              value={input.email}
+              onChange={handleChanges}
+              placeholder="example@example.com"
+            />
+          </FormGroup>
+          <FormGroup>
+          <Form.Label>Phone Password</Form.Label>
+            <Form.Control
+             required
+              type="password"
+              name="password"
+              value={input.password}
+              onChange={handleChanges}
+              placeholder="********"
+            />
+          </FormGroup>
+        <Button className="subutton">Login</Button>
+      </Form>
+    </div>
+  );
+}
---
+++ frontend/src/pages/SignUp.js
@@ -0,0 +1,91 @@
+import React, { useState } from "react";
+import { Form, Col, Button,FormGroup} from "react-bootstrap";
+import { useHistory, Redirect } from "react-router-dom";
+import axios from "axios";
+import './SignUp.css'
+export const SignUp = () => {
+
+  const[userinput, setUserInput]=useState({
+    username:"",
+    email:"",
+    password:""
+  })
+
+     
+
+    const handleFieldChange = e => {
+      const {name,value}=e.target;
+      setUserInput({
+        ...userinput,
+        [name]:value,
+        
+      });
+    }
+
+    const handleSubmit = e => {
+      e.preventDefault();
+     
+    const requestConfig={
+      url:'http://127.0.0.1:4001/api/v1/signup',
+      method:'post',
+      header:{'Content-Type': 'application/jason'},
+      data:{
+        username:userinput.username,
+        email:userinput.email,
+        password:userinput.password,
+      },
+    };
+    axios(requestConfig)
+    .then((response)=>{
+      console.log(response);
+    })
+     .catch((err)=>{
+       console.log(`${err}`);
+     });
+
+    }
+
+
+    return (  
+    <div className="App">
+         
+        <Form className="form" onSubmit={handleSubmit}>
+        <h2 className="title">SignUp</h2>
+          <FormGroup>
+          <Form.Label>Enter Username</Form.Label>
+            <Form.Control
+              required
+              type="username"
+              name="username"
+              value={userinput.username}
+              onChange={handleFieldChange}
+              placeholder="Exname"
+            />
+          </FormGroup>
+          <FormGroup>
+          <Form.Label>Enter Email</Form.Label>
+            <Form.Control
+              required
+              type="email"
+              name="email"
+              value={userinput.email}
+              onChange={handleFieldChange}
+              placeholder="example@example.com"
+            />
+          </FormGroup>
+          <FormGroup>
+          <Form.Label>Enter Password</Form.Label>
+            <Form.Control
+              required
+              type="password"
+              name="password"
+              value={userinput.password}
+              onChange={handleFieldChange}
+              placeholder="********"
+            />
+          </FormGroup>
+        <Button className="subutton">Signup</Button>
+      </Form>
+    </div>
+    );
+}
